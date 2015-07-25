c550e4a349510f10d17eb4d54677887446b2b4bc
Nanme <real90@hotmail.fr>
2015-07-25 15:38:04 +0200

    Complete Category class.

--- src/customCharacter/Category.java
+++ src/customCharacter/Category.java
@@ -10,8 +10,9 @@ import java.util.ArrayList;
  */
 public class Category extends AbstractCategory{
 
-	private ArrayList<AbstractCategory> val;
+	private ArrayList<AbstractCategory> content;
 	
+	//Utility
 	/**
 	 * Find the index of the AbstractCategory element which have name. Return -1 if it do not find anything.
 	 * @param name
@@ -21,39 +22,85 @@ public class Category extends AbstractCategory{
 	 */
 	private int find(String name){
 		int index = 0;
-		while(index < val.size()){
-			if (val.get(index).name == name) return index;
+		while(index < content.size()){
+			if (content.get(index).name == name) return index;
 			++index;
 		}
 		return -1;
 	}
 	
 	//Builder
+	/**
+	 * Build a void Category with its name.
+	 * @param name
+	 * 		NAme of the category.
+	 */
 	public Category(String name){
 		this.name = new String(name);
+		this.content = new ArrayList<AbstractCategory>();
 	}
-	
+
+	/**
+	 * Build a Category with a Name and an ArrayList of AbstractCategory.
+	 * @param name
+	 * 		Name of the Category.
+	 * @param val
+	 * 		ArrayList of AbstractCategory. The future contents of the category.
+	 */
 	public Category(String name, ArrayList<AbstractCategory> val){
 		this.name = new String(name);
-		this.val = new ArrayList<AbstractCategory>(val);
+		this.content = new ArrayList<AbstractCategory>(val);
+	}
+	/**
+	 * Build a Category with its and its contents.
+	 * The content is a variable number of AbstractCategory Object.
+	 * @param name
+	 * 		Name of the category.
+	 * @param elts
+	 * 		The elements to add.
+	 */
+	public Category(String name, AbstractCategory...  elts){
+		this.name = new String(name);
+		this.content = new ArrayList<AbstractCategory>();
+		for(AbstractCategory it : elts){
+			this.content.add(it);
+		}
 	}
 	
+	//Getter & Setters
+	/**
+	 * Get the content of the category.
+	 * @return content
+	 * 		Content of the category.
+	 */
+	public ArrayList<AbstractCategory> getContent(){
+		return this.content;
+	}
+	/**
+	 * Set the content of the Category.
+	 * @param content
+	 * 		Content to set.
+	 */
+	public void setContent(ArrayList<AbstractCategory> content) {
+		this.content = content;
+	}
+
+	//Manipulation
 	/**
 	 * Add a new element in the category
 	 * @param elt
 	 * 		Element to add.
 	 */
 	public void addElement(AbstractCategory elt){
-		val.add(elt);
+		content.add(elt);
 	}
-	
 	/**
 	 * Remove an AbastractCategory element.
 	 * @param elt
 	 * 		Element to remove.
 	 */
 	public void deleteElement(AbstractCategory elt){
-		val.remove(elt);
+		content.remove(elt);
 	}
 	/**
 	 * Delete the first AbstractCategory which have name as attribute.
@@ -63,7 +110,7 @@ public class Category extends AbstractCategory{
 	 * 		true if succeed. false if not.
 	 */
 	public boolean deleteElement(String name){
-		if (val.remove(find(name)) != null) return true;
+		if (content.remove(find(name)) != null) return true;
 		else return false;
 	}
 	
