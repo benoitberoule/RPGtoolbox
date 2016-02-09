ad3f83c1494cc3feea209960179dda0fe4efb043
PRuffy <pierre.ruffy@gmail.com>
2016-02-09 13:44:10 +0100

    Début controlleur de la fenêtre de simulateur de dé

---
+++ src/gui/rollDice/RollDiceFrameController.java
@@ -0,0 +1,51 @@
+package gui.rollDice;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RollDiceFrameController {
+
+    @FXML
+    private Label d4Label;
+
+    @FXML
+    private Label d6Label;
+
+    @FXML
+    private Label d8Label;
+
+    @FXML
+    private Label d10Label;
+
+    @FXML
+    private Label d12Label;
+
+    @FXML
+    private Label d20Label;
+
+    @FXML
+    private Label d100Label;
+
+    @FXML
+    private TextField d4TextField;
+
+    @FXML
+    private TextField d6TextField;
+
+    @FXML
+    private TextField d8TextField;
+
+    @FXML
+    private TextField d10TextField;
+
+    @FXML
+    private TextField d12TextField;
+
+    @FXML
+    private TextField d20TextField;
+
+    @FXML
+    private TextField d100TextField;
+}
